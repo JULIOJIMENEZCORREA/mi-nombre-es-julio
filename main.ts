d592cefdf1e86f6e5f864ecce83efd55850d772e
JULIOJIMENEZCORREA <90298028+JULIOJIMENEZCORREA@users.noreply.github.com>
2021-09-07 22:21:19 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,60 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(4000)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+})
